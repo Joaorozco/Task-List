2175c9c1e0fd9c18daa2d5c516417276d14b0e96
Joaquin Orozco <joaquinalexisorozco@gmail.com>
2022-03-18 17:19:45 -0300

    Servicio y servidor de tareas listo

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Task } from 'src/app/Task';
-import { TASKS } from 'src/app/mock-task';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { TaskService } from 'src/app/service/task.service';
+import { Task } from '../../Task'
 
 
 @Component({
@@ -10,11 +9,16 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
-  faTimes = faTimes;
-  constructor() { }
+  tasks: Task[] = [];
+
+  constructor(
+    private taskService: TaskService
+  ) {}
 
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe( tasks => {
+      this.tasks = tasks
+    });
   }
 
 }
