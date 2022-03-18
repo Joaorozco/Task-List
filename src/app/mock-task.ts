f94777757df8561d8e7d6f8762348a7e601c5849
Joaquin Orozco <joaquinalexisorozco@gmail.com>
2022-03-18 14:25:55 -0300

    second commit

---
+++ src/app/mock-task.ts
@@ -0,0 +1,28 @@
+import { Task } from "./Task"
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: "Terminar primer modulo de Angular",
+    day: 'Agosto 5 a las 12:00',
+    reminder: true
+  },
+  {
+    id: 2,
+    text: "Almorzar",
+    day: 'Agosto 5 a las 14:00',
+    reminder: true
+  },
+  {
+    id: 2,
+    text: "Terminar primer modulo de Angular",
+    day: 'Agosto 5 a las 18:00',
+    reminder: true
+  },
+  {
+    id: 2,
+    text: "invertigar bootstra",
+    day: 'Agosto 5 a las 15:00',
+    reminder: true
+  },
+]
