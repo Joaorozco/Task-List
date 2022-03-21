8be782f684ed83efb59bde9f632c64d7a30a43e1
Joaquin Orozco <joaquinalexisorozco@gmail.com>
2022-03-21 10:21:37 -0300

    Add Task

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -37,4 +37,10 @@ export class TasksComponent implements OnInit {
 
   }
 
+  addTask(task:Task){
+    this.taskService.addTask(task).subscribe((task) => {
+      this.tasks.push(task)
+    })
+  }
+
 }
